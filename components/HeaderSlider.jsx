7a81d2cfa5a8589334a62a95a90c9ff4f88440d1
Ian S <57616983+Ian-Coder1000001@users.noreply.github.com>
2025-04-10 22:34:28 +0300

    Update HeaderSlider.jsx

--- components/HeaderSlider.jsx
+++ components/HeaderSlider.jsx
@@ -57,7 +57,7 @@ const HeaderSlider = () => {
             className="flex flex-col-reverse md:flex-row items-center justify-between bg-[#E6E9F2] py-8 md:px-14 px-5 mt-6 rounded-xl min-w-full"
           >
             <div className="md:pl-8 mt-10 md:mt-0">
-              <p className="md:text-base text-orange-600 pb-1">{slide.offer}</p>
+              <p className="md:text-base text-blue-600 pb-1">{slide.offer}</p>
               <h1 className="max-w-lg md:text-[40px] md:leading-[48px] text-2xl font-semibold">
                 {slide.title}
               </h1>
@@ -88,7 +88,7 @@ const HeaderSlider = () => {
             key={index}
             onClick={() => handleSlideChange(index)}
             className={`h-2 w-2 rounded-full cursor-pointer ${
-              currentSlide === index ? "bg-orange-600" : "bg-gray-500/30"
+              currentSlide === index ? "bg-blue-600" : "bg-gray-500/30"
             }`}
           ></div>
         ))}
